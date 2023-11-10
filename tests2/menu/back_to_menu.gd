3bfe2db74d8db4b2ed98a5d4a683cf13bf0a2ada
KacperTZSTI <113526043+KacperTZSTI@users.noreply.github.com>
2023-11-10 17:38:54 +0100

    You can now escape the options menu.

--- tests2/menu/back_to_menu.gd
+++ tests2/menu/back_to_menu.gd
@@ -1,14 +1,11 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_tree().change_scene_to_file("res://menu/memu.tscn")
+
 
 func _on_back_pressed():
 	get_tree().change_scene_to_file("res://menu/memu.tscn")
