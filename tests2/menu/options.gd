904aae2e371c8f765ace0f9ddcce35eeb52e9d3f
KacperTZSTI <113526043+KacperTZSTI@users.noreply.github.com>
2023-11-10 20:55:06 +0100

    kapa: press the esc key to exit

--- tests2/menu/options.gd
+++ tests2/menu/options.gd
@@ -22,7 +22,8 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_tree().change_scene_to_file("res://menu/memu.tscn")
 
 func _on_back_pressed():
 	get_tree().change_scene_to_file("res://menu/memu.tscn")
