1221fd438a1d6b40672dbb6c4977982e39eba378
1p22geo <1p22geodecki@gmail.com>
2023-11-11 12:10:55 +0100

    feat: timer to kick your a$s back to stage select

--- gwj-63/levels/level_01/CharacterBody2D.gd
+++ gwj-63/levels/level_01/CharacterBody2D.gd
@@ -4,15 +4,24 @@ var FRICTION = 50
 var ACCELERATION = 40
 var MAX_SPEED = 250
 var input_vector = Vector2.ZERO
+var MAX_TIME = 20
+@export
+var time = MAX_TIME
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	time = MAX_TIME;
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	time -= delta
+	get_tree().get_nodes_in_group("Global indicators")[0].text = "Time left: "+str(int(time))
+	if time<0:
+		get_tree().change_scene_to_file("res://menu/stage_select.tscn")
 	movement(delta)
+	
 	
 
 func movement(delta):
