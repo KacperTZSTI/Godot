ecb26a450af12eee8a10f7837b3cd541849c07ce
1p22geo <1p22geodecki@gmail.com>
2023-11-11 11:16:21 +0100

    feat: the character rotates with the direction of movement

--- gwj-63/levels/level_01/player.gd
+++ gwj-63/levels/level_01/player.gd
@@ -16,6 +16,7 @@ func _physics_process(delta):
 	var velocity = Vector2.ZERO
 
 	if Input.is_action_pressed('ui_up'):
+		
 		velocity.y -= speed
 	if Input.is_action_pressed('ui_left'):
 		velocity.x -= speed
@@ -26,3 +27,4 @@ func _physics_process(delta):
 		
 	
 	apply_impulse(velocity, Vector2.ZERO)
+	rotation = linear_velocity.angle()
