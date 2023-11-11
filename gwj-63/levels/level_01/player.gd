c22370c954427133c63722731b61a0322d26341d
1p22geo <1p22geodecki@gmail.com>
2023-11-11 09:42:05 +0100

    feat: character movement

---
+++ gwj-63/levels/level_01/player.gd
@@ -0,0 +1,28 @@
+extends RigidBody2D
+
+var speed = 3.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+
+func _physics_process(delta):
+	var velocity = Vector2.ZERO
+
+	if Input.is_action_pressed('ui_up'):
+		velocity.y -= speed
+	if Input.is_action_pressed('ui_left'):
+		velocity.x -= speed
+	if Input.is_action_pressed('ui_down'):
+		velocity.y += speed
+	if Input.is_action_pressed('ui_right'):
+		velocity.x += speed
+		
+	
+	apply_impulse(velocity, Vector2.ZERO)
