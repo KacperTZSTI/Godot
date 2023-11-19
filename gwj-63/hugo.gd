95331695c9a4b969dc9c0306063cd5a20bb450f7
1p22geo <1p22geodecki@gmail.com>
2023-11-19 11:40:55 +0100

    hugo.

--- gwj-63/hugo.gd
+++ gwj-63/hugo.gd
@@ -10,8 +10,11 @@ func _on_area_2d_body_entered(body):
 			body.time = -9
 			for item in get_tree().get_nodes_in_group("Game over items"):
 				item.visible = true
-			body.get_tree().get_nodes_in_group("Game over items")[0].position.y = -100
-			body.get_tree().get_nodes_in_group("Game over items")[0].text  = "Why are you here \nYou shouldn't wander at night\n Have you forgotten your way to our home?\nIt's in the stage select\n Please look for it.\n\n\n\n __g4__"
+			# body.get_tree().get_nodes_in_group("Game over items")[3].position.y = 50
+			get_tree().get_nodes_in_group("Game over items")[0].queue_free()
+			get_tree().get_nodes_in_group("Game over items")[3].visible = true
+			
+			body.get_tree().get_nodes_in_group("Game over items")[3].text  = "Why are you here \nYou shouldn't wander at night\n Have you forgotten your way to our home?\nIt's in the stage select\n Please look for it.\n\n\n\n\n __g4__"
 
 func _process(delta):
 	time+= delta
