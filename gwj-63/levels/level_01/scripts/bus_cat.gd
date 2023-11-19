b13f20d35cd8a8582c2ecbc08eee692e6f3fa15f
1p22geo <1p22geodecki@gmail.com>
2023-11-19 12:10:32 +0100

    fix: bus cat fixed

--- gwj-63/levels/level_01/scripts/bus_cat.gd
+++ gwj-63/levels/level_01/scripts/bus_cat.gd
@@ -3,13 +3,13 @@ extends "res://levels/koteł.gd"
 var time = 0;
 var TIME_VISIBLE = 91
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Area2D/CollisionShape2D.disabled = true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	time += delta;
 	if time > TIME_VISIBLE:
+		$Area2D/CollisionShape2D.disabled = false
 		visible = true
